32563121e03a4a742e3ffe2ceb7c9bdaf56a7451
Kyuho Kim <valve.god.project@gmail.com>
2020-03-09 21:59:12 +0900

    created

---
+++ rgbledmediaart.ino
@@ -0,0 +1,25 @@
+int led[3] = {15, 4, 17};
+int gnd[3] = {2, 16, 5};
+
+void setup() {
+  // put your setup code here, to run once:
+  for (int i=0; i<3; i++) {
+    pinMode(led[i], OUTPUT);
+    digitalWrite(led[i], 1);
+    
+    pinMode(gnd[i], OUTPUT);
+    digitalWrite(gnd[i], 0);
+  }
+  Serial.begin(115200);
+}
+
+void loop() {
+  static int j=30;
+  // put your main code here, to run repeatedly:
+  for (int k=0; k<3; k++) {
+    digitalWrite(led[k], 1);
+    delay(50);
+    digitalWrite(led[k], 0);
+    delay(100);
+  }
+}
