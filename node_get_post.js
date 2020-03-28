858525a0ac1b9400f0150201b74b2fb1d963db23
Kyuho Kim <valve.god.project@gmail.com>
2020-03-29 01:41:09 +0900

    Update node_get_post.js

--- node_get_post.js
+++ node_get_post.js
@@ -4,6 +4,7 @@ app.use(express.json())
 require('date-utils')
 port = 8000
 
+// http://localhost:8000/capston2020?name=kim&color=red
 app.get('/capston2020', function(req, res) {
     r = req.query
     r.ip = req.ip.replace(/^.*:/, '')
@@ -24,6 +25,7 @@ app.post('/capston2020', function(req, res) {
     res.send(JSON.stringify(r))
 })
 
+// http://localhost:8000/capston2020/kim/red
 app.get('/capston2020/:a/:b', function(req, res) {
     r = req.params
     ip = req.ip.replace(/^.*:/, '')
@@ -33,3 +35,5 @@ app.get('/capston2020/:a/:b', function(req, res) {
     console.log(r)
     res.send(JSON.stringify(r))
 })
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
